b3dd7af537d3e442958e2502d4ed4380438bb930
Alexander Keller <ak@aflx.de>
2011-11-29 11:16:09 +0100

    downloader now returns FileEntry, interface changes

--- Downloader.java
+++ Downloader.java
@@ -2,87 +2,116 @@
  * aflx - always flexible
  * http://www.aflx.de
  * ak@aflx.de
- * 
+ *
  * I have adapted the classname and signature of the method of this plugin for
  * Android and iOS.
- * 
+ *
  * Copyright 2011 Alexander Keller
  * All Rights Reserved.
- * 
- * Thanks to Mauro Rocco 
+ *
+ * Thanks to Mauro Rocco
  * http://www.toforge.com
  */
 
-package de.aflx.phonegap.plugins;
- 
+package de.aflx.phonegap.plugins.Downloader;
+
 import org.json.JSONArray;
 import org.json.JSONException;
- 
+import org.json.JSONObject;
+
 import android.util.Log;
- 
+
 import com.phonegap.api.Plugin;
 import com.phonegap.api.PluginResult;
- 
+
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
- 
+
 public class Downloader extends Plugin{
- 
-	@Override
-	public PluginResult execute(String action, JSONArray args, String callbackId) {
-		if (action.equals("downloadFile")) {
-			try {
-				return this.downloadFile(args.getString(0),args.getString(1),args.getString(2));
-			} catch (JSONException e) {
-				return new PluginResult(PluginResult.Status.ERROR, "Param errrors");
-			}
-		} else {
-			return new PluginResult(PluginResult.Status.INVALID_ACTION);
-		}
-	}
- 
-	private PluginResult downloadFile(String fileUrl, String dirName, String fileName){
-		try {
-			File dir = new File(dirName);
-			if(!dir.exists()){
-				Log.d("DownloaderPlugin", "directory "+dirName+" created");
-				dir.mkdirs();
-			}
- 
-			File file = new File(dirName+fileName);
-			URL url = new URL(fileUrl);
-			HttpURLConnection ucon = (HttpURLConnection) url.openConnection();
-			ucon.setRequestMethod("GET");
-			ucon.setDoOutput(true);
-			ucon.connect();
- 
-			Log.d("DownloaderPlugin", "download url:" + url);
- 
-			InputStream is = ucon.getInputStream();
-			byte[] buffer = new byte[1024];
-			int len1 = 0;
- 
-			FileOutputStream fos = new FileOutputStream(file);
- 
-			while ( (len1 = is.read(buffer)) > 0 ) {
-				fos.write(buffer,0, len1);
-			}
- 
-			fos.close();
- 
-			Log.d("DownloaderPlugin", "Downloaded file " + dirName+fileName);
- 
-		} catch (IOException e) {
- 
-			Log.d("DownloaderPlugin", "Error: " + e);
-			return new PluginResult(PluginResult.Status.ERROR, "Error: " + e);
- 
-		}
- 
-		return new PluginResult(PluginResult.Status.OK, dirName+fileName);
-	}
+
+    private static final String LOG_TAG = "Downloader";
+
+    @Override
+    public PluginResult execute(String action, JSONArray args, String callbackId) {
+        if (action.equals("download")) {
+            String file = null;
+            String server = null;
+
+            try {
+                file = args.getString(0);
+                server = args.getString(1);
+            }
+            catch (JSONException e) {
+                Log.d(LOG_TAG, "Missing filename or server name");
+                return new PluginResult(PluginResult.Status.JSON_EXCEPTION, "Missing filename or server name");
+            }
+
+            try {
+                JSONObject r = download(file, server);
+                Log.d(LOG_TAG, "****** About to return a result from download");
+                return new PluginResult(PluginResult.Status.OK, r, "window.localFileSystem._castEntry");
+            } catch (Exception e) {
+                return new PluginResult(PluginResult.Status.ERROR, "Param errrors");
+            }
+        } else {
+            return new PluginResult(PluginResult.Status.INVALID_ACTION);
+        }
+    }
+
+    /**
+     * Downloads a file form a given URL and saves it to the specified directory.
+     *
+     * @param server        URL of the server to receive the file
+     * @param file      	Full path of the file on the file system
+     * @return JSONObject 	the downloaded file
+     */
+    public JSONObject download(String filePath, String sourceUrl) throws IOException {
+        try {
+            File file = new File(filePath);
+
+            // create needed directories
+            file.getParentFile().mkdirs();
+
+            // connect to server
+            URL url = new URL(sourceUrl);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setDoOutput(true);
+            connection.connect();
+
+            Log.d(LOG_TAG, "Download file:" + url);
+
+            InputStream inputStream = connection.getInputStream();
+            byte[] buffer = new byte[1024];
+            int bytesRead = 0;
+
+            FileOutputStream outputStream = new FileOutputStream(file);
+
+            // write bytes to file
+            while ( (bytesRead = inputStream.read(buffer)) > 0 ) {
+                outputStream.write(buffer,0, bytesRead);
+            }
+
+            outputStream.close();
+
+            Log.d(LOG_TAG, "Saved file: " + filePath);
+
+            // create FileEntry object
+            JSONObject entry = new JSONObject();
+
+            entry.put("isFile", file.isFile());
+            entry.put("isDirectory", file.isDirectory());
+            entry.put("name", file.getName());
+            entry.put("fullPath", file.getAbsolutePath());
+
+            return entry;
+        } catch (Exception e) {
+            Log.d(LOG_TAG, e.getMessage(), e);
+            throw new IOException("Error while downloading");
+        }
+    }
 }
--- Downloader.js
+++ Downloader.js
@@ -2,17 +2,26 @@
  * aflx - always flexible
  * http://www.aflx.de
  * ak@aflx.de
- * 
+ *
  * Copyright 2011 Alexander Keller
  * All Rights Reserved.
  */
 
 function Downloader() {
 }
- 
-Downloader.prototype.downloadFile = function(fileUrl, dirName, fileName, win, fail) {
-    console.log("downloadFile(" + fileUrl + ", " + dirName + fileName + ")");
-	
-	PhoneGap.exec(win, fail, "Downloader", "downloadFile", [fileUrl, dirName, fileName]);
+
+/**
+ * Downloads a file form a given URL and saves it to the specified directory.
+ * @param sourceUrl {String}          URL of the server to receive the file
+ * @param targetFile {String}         Full path of the file on the device
+ * @param successCallback (Function}  Callback to be invoked when upload has completed
+ * @param errorCallback {Function}    Callback to be invoked upon error
+ */
+Downloader.prototype.downloadFile = function(sourceUrl, targetFile, successCallback, errorCallback) {
+    PhoneGap.exec(successCallback, errorCallback, 'Downloader', 'download', [targetFile, sourceUrl]);
 };
+
+PhoneGap.addConstructor(function() {
+    PhoneGap.addPlugin("downloader", new Downloader());
+});
 
